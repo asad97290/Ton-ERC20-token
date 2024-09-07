23c7c21c391b9c107a523e699462804aae2a6bd0
asadullah khan <asadmuhammad427@gmail.com>
2024-09-07 23:12:34 +0500

    fix test

--- tests/KKFI.spec.ts
+++ tests/KKFI.spec.ts
@@ -1,43 +1,34 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Address, Cell, toNano } from '@ton/core';
+import { Address, beginCell, Slice, toNano } from '@ton/core';
 import { KKFI } from '../wrappers/KKFI';
 import '@ton/test-utils';
+import { JettonDefaultWallet } from '../build/KKFI/tact_JettonDefaultWallet';
+import { buildOnchainMetadata } from '../utils/jetton-helpers';
 
 describe('KKFI', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
     let receiver: SandboxContract<TreasuryContract>;
     let kKFI: SandboxContract<KKFI>;
+    let jettonWallet: SandboxContract<JettonDefaultWallet>;
+    const jettonParams = {
+        name: "Best Practice",
+        description: "This is description of Test tact jetton",
+        symbol: "XXXE",
+        image: "https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn",
+    };
+    let content = buildOnchainMetadata(jettonParams)
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         blockchain = await Blockchain.create();
-        const cell = new Cell();
+        
+        const cell = content;
+        
         deployer = await blockchain.treasury('deployer');
         receiver = await blockchain.treasury('receiver');
+
         kKFI = blockchain.openContract(await KKFI.fromInit(cell));
 
-
-        const deployResult = await kKFI.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
-        );
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: kKFI.address,
-            deploy: true,
-            success: true,
-        });
-    });
-
-    it('should check owner address', async () => {
-        const owner = await kKFI.getOwner()
 
 
         const deployResult = await kKFI.send(
@@ -47,7 +38,49 @@ describe('KKFI', () => {
             },
             {
                 $$type: 'Mint',
-                amount: 1n,
+                amount: toNano("1"),
+                receiver:receiver.address
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: kKFI.address,
+            deploy: true,
+            success: true,
+        });
+
+
+     
+
+        
+
+    });
+
+    it('should check owner address', async () => {
+        const owner = await kKFI.getOwner()
+        expect(owner).toEqualAddress(deployer.address);
+    });
+
+    it("Test: whether contract deployed successfully", async () => {
+        expect((await kKFI.getGetJettonData()).owner).toEqualAddress(deployer.address)
+        expect((await kKFI.getGetJettonData()).totalSupply).toEqual(toNano("1"))
+        expect((await kKFI.getGetJettonData()).max_supply).toEqual(toNano("1000000000"))
+        expect((await kKFI.getGetJettonData()).content).toEqualCell(content)
+        expect((await kKFI.getGetJettonData()).mintable).toEqual(true)
+    });
+
+    it('should mint', async () => {
+        const mintAmount =  toNano("10");
+        const oldTotalSupply = (await kKFI.getGetJettonData()).totalSupply;
+        const deployResult = await kKFI.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Mint',
+                amount: mintAmount,
                 receiver:receiver.address
             }
         );
@@ -56,10 +89,57 @@ describe('KKFI', () => {
             to: kKFI.address,
             success: true,
         });
-        expect(owner).toEqualAddress(deployer.address);
-        expect((await kKFI.getGetJettonData()).totalSupply).toEqual(1n);
-        expect((await kKFI.getGetJettonData()).mintable).toEqual(true)
-        
+        expect((await kKFI.getGetJettonData()).totalSupply).toEqual(oldTotalSupply+mintAmount);
+
+
+
+        const playerWallet = await kKFI.getGetWalletAddress(receiver.address);
+        jettonWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(playerWallet));
+
+
+        const walletData = await jettonWallet.getGetWalletData();
+        expect(walletData.owner).toEqualAddress(receiver.address);
+        expect(walletData.balance).toBeGreaterThanOrEqual(mintAmount);
+    })
+    
+    it('wallet owner should be able to send jettons', async () => {
+        const playerWallet = await kKFI.getGetWalletAddress(receiver.address);
+        jettonWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(playerWallet));
+        let forwardPayload = beginCell().storeUint(0x1234567890abcdefn, 128).endCell().asSlice();
+
+        const deployResult = await jettonWallet.send(
+            receiver.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'TokenTransfer',
+                queryId :0n,
+                amount :toNano('5'),
+                destination:deployer.address,
+                response_destination:deployer.address,
+                custom_payload:null,
+                forward_ton_amount:0n,
+                forward_payload: forwardPayload
+                
+            }
+        );
+        expect(deployResult.transactions).toHaveTransaction({
+            from: receiver.address,
+            to: jettonWallet.address,
+            success: true,
+        });
+
+
+
+        const receiverWallet = await kKFI.getGetWalletAddress(deployer.address);
+        jettonWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(receiverWallet));
+
+
+        const walletData = await jettonWallet.getGetWalletData();
+        expect(walletData.owner).toEqualAddress(deployer.address);
+        expect(walletData.balance).toBeGreaterThanOrEqual(toNano('5'));
+
     });
 
 
